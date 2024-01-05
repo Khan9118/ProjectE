9d064e1d10447b891fdea929af5a8f8d75a0f73a
maboo <maboo@Mabood.myfiosgateway.com>
2024-01-05 17:31:38 -0500

    Initial Commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,12 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Java is My habit");
+		
+		System.out.println("Batch 11 Java Learning Project");
+	}
+
+}
